13b0f43df17fc3692970d61ea51b77db16479ea4
Shadow <94984080+TrueShadow01@users.noreply.github.com>
2026-07-20 23:34:54 +0200

    Add asset browser contents and THIRD_PART_NOTICES.md

---
+++ modules/asset_browser/Resources/Scripts/initializeLibrary.py
@@ -0,0 +1,7 @@
+#Author: NSA Cloud
+import bpy
+
+bpy.ops.re_asset.initialize_library()
+
+bpy.ops.wm.save_mainfile()
+bpy.ops.wm.quit_blender()
---
+++ modules/asset_browser/__init__.py
@@ -0,0 +1,12 @@
+"""
+RE Asset Browser integration for REME.
+
+Adapted from NSACloud/RE-Asset-Library
+Source commit: 4d04bde69693b2eb613fb0b9625a2dc3282f74d5
+"""
+
+def register():
+    pass
+
+def unregister():
+    pass
